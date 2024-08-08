d522287442fb76ee484de437021a4db1ae0b5581
moarray28 <moreritesh3969@gmail.com>
2024-08-08 22:33:49 +0530

    updating server.js file

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
@@ -7,16 +7,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Enable CORS
+app.use(cors());
 app.use(express.json());
 
-const corsOptions = {
-    origin: 'https://weatherapp-delta-kohl.vercel.app', // Replace with your frontend URL
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  };
-  
 
-  app.use(cors(corsOptions));
 
 app.get('/weather', async (req, res) => {
     const { location } = req.query;
@@ -41,32 +35,3 @@ console.log('API Key:', process.env.VITE_API_KEY);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-*/
-import axios from 'axios';
-
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const { location } = req.query;
-    const apiKey = process.env.VITE_API_KEY; // Adjust this as needed
-
-    let url;
-    if (location) {
-      url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=6`;
-    } else {
-      url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Vasai&days=6`;
-    }
-
-    try {
-      const response = await axios.get(url);
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Error fetching weather data' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
-
